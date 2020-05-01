2675f82e5224c3c547812920eb747e2edc9a3aa2
amiba2018 <otukaresama2004@gmail.com>
2020-05-01 15:31:28 +0900

    #6 increase the pattern of words in seeder class

--- database/seeds/WordsTableSeeder.php
+++ database/seeds/WordsTableSeeder.php
@@ -18,14 +18,20 @@ class WordsTableSeeder extends Seeder
             ['word' => '1:さすが！！百年に1人の逸材！', 'created_at' => $now, 'updated_at' => $now],
             ['word' => '1:ナイス×3！', 'created_at' => $now, 'updated_at' => $now],
             ['word' => '1:全てにおいて神がかり的！', 'created_at' => $now, 'updated_at' => $now],
-            ['word' => '1:すこぶる調子いい！', 'created_at' => $now, 'updated_at' => $now],
+            ['word' => '1:すこぶる調子いいね！', 'created_at' => $now, 'updated_at' => $now],
             ['word' => '1:抜きんでている！', 'created_at' => $now, 'updated_at' => $now],
             //関西弁
             ['word' => '2:ええやん!', 'created_at' => $now, 'updated_at' => $now],
             ['word' => '2:ごっつええやん!', 'created_at' => $now, 'updated_at' => $now],
             ['word' => '2:今日はどうしたん？えらいシュッとしているやん！', 'created_at' => $now, 'updated_at' => $now],
             ['word' => '2:自分おもろいやつやな！', 'created_at' => $now, 'updated_at' => $now],
-            ['word' => '2:アンタにはかなわんわ！', 'created_at' => $now, 'updated_at' => $now],
+            ['word' => '2:すてきやん', 'created_at' => $now, 'updated_at' => $now],
+            //殿様
+            ['word' => '3:大儀であった!', 'created_at' => $now, 'updated_at' => $now],
+            ['word' => '3:あっぱれ!', 'created_at' => $now, 'updated_at' => $now],
+            ['word' => '3:よきかな♪ よきかな♪', 'created_at' => $now, 'updated_at' => $now],
+            ['word' => '3:お主やりよるな', 'created_at' => $now, 'updated_at' => $now],
+            ['word' => '3:ほめてつかわす！！', 'created_at' => $now, 'updated_at' => $now],
         ];
         DB::table('words')->insert($data);
     }
