b09d342f4e3761cd78d09f9121a1e4a45513609d
amiba2018 <otukaresama2004@gmail.com>
2020-05-04 19:25:19 +0900

    #14 Remove unused method in class User

--- app/User.php
+++ app/User.php
@@ -12,33 +12,6 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
     //メンションがある場合はwebhookで返す
     public static function getWordSend($request) {
         $mention = Word::getMention($request);
